fd30c47d3f3d5e06a631f8b833fce102abdee3c7
Alexey <alexey-nt@mail.ru>
2023-10-12 18:09:59 +0500

    HW_4

---
+++ Test/seminars/fourth/MainTest.java
@@ -0,0 +1,39 @@
+package seminars.fourth;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+class MainTest {
+
+    @Test
+    public void simpleTest() {
+        // Создаем мок
+        List<String> mockedList = mock(List.class);
+
+        // Используем мок
+        mockedList.add("one");
+        mockedList.clear();
+
+        // Проверяем, что методы были вызваны
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+    }
+
+    @Test
+    public void iteratorWillReturnHelloWorld() {
+        // Arrange
+        Iterator iteratorMock = mock(Iterator.class);
+        when(iteratorMock.next()).thenReturn("Hello", "World");
+        // Act
+        String result = iteratorMock.next() + " " + iteratorMock.next();
+        // ...
+        assertEquals("Hello World", result);
+    }
+
+}
